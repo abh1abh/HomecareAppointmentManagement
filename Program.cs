713055c211d8cdf07fc77fec1e7839a486ee2999
alexanderhoyskel <1664AlfredAnna>
2025-09-25 00:30:47 +0200

    .

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Serilog;
 using Serilog.Events;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("AppDbContextConnection") ?? throw new
